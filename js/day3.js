038d762530c923bd31a24ca16a1eec15b6528eb7
HyunRyuuu <ryubifa@gmail.com>
2022-01-13 01:33:08 +0900

    TIL
    	#1. script 요소 태그는 작동할 태그 아래에 위치해야 한다.

---
+++ js/day3.js
@@ -0,0 +1,17 @@
+function changeText_mo(){
+    var text = document.getElementsByClassName('mo');
+
+    text[0].innerHTML = "모두 모여라"
+}
+
+function changeText_gak(){
+    var text = document.getElementsByClassName('gak');
+
+    text[0].innerHTML = "각잡고"
+}
+
+function changeText_co(){
+    var text = document.getElementsByClassName('co');
+
+    text[0].innerHTML = "코딩공부 하자"
+}
