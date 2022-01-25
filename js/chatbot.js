f96bd05dded707b6b38bb0af5c7447828823f4ed
HyunRyuuu <ryubifa@gmail.com>
2022-01-25 23:45:04 +0900

    챗봇 만들기 2 - "잘 자" case 만들기

--- js/chatbot.js
+++ js/chatbot.js
@@ -1,18 +1,14 @@
-// TIL
-//#1. 챗봇
-//    - 기본적인 챗봇은 조건문을 통해 유저가 특정 질문을 던졌을 때, 그에 맞는 대답을 해주는 것
-
-
 function checkText() {
     var value = document.getElementById("orderBox").value;
-    console.log(value);
-
 
     var chatBox = document.getElementById("chatBox");
-    if(value == "안녕") {
-        console.log(chatBox);
-        chatBox.innerHTML = "Hii";
+    var character = document.getElementById("character");
+    if( value == "잘 자" || "잘자" ) {
+        chatBox.innerHTML = "晚安!";
+        chatBox.style.color = "#fff";
+        character.src = "../img/lulu_sleep.png";
+        document.body.style.background = "#322938";
     } else {
-        console.log(value);
+        chatBox.innerHTML = "등록되지 않은 명령어 입니다.";
     }
 }
